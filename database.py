62513cd26b44c35fde8276fdbba5cacd555d6620
mohammadrad <mohammadafzalirad2001@gmail.com>
2026-07-27 15:55:28 +0330

    deleting the docker files

--- database.py
+++ database.py
@@ -3,9 +3,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://abzil:abzil123@pgdb:5432/imsdb"
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./imsapp.db'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
