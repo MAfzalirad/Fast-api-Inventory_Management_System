3f9e28622dda73eb8da30517df9cb4123d74194e
mohammadrad <mohammadafzalirad2001@gmail.com>
2026-08-01 20:56:09 +0330

    add alembic as a container

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from routers import items, auth, users, admin
 
 app = FastAPI(title="Inventory Management API")
 
-models.Base.metadata.create_all(bind=engine)
-
 app.include_router(items.router)
 app.include_router(auth.router)
 app.include_router(users.router)
